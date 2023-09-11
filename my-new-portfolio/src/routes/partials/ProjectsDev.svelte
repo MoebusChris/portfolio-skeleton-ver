8945a68b1cc14f32ed2a8a7b8c95af43118a00bf
Subeom Jeong <92135821+MoebusChris@users.noreply.github.com>
2023-09-10 22:09:43 -0700

    Working on responsive card design.

--- my-new-portfolio/src/routes/partials/ProjectsDev.svelte
+++ my-new-portfolio/src/routes/partials/ProjectsDev.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { getImageLink } from "$lib/images";
     import { cardLists } from "$lib/card";
-
+    import { slide } from "svelte/transition";
 
 
 
@@ -12,9 +12,28 @@
         Development
     </h3>
     <section class="grid grid-cols-2 md:grid-cols-3 gap-4 py-8">
-    	<div>
-    		<img class="h-auto max-w-full rounded-lg" src={getImageLink({ id: 'qucYweSE3ZY', w: 1280, h: 1280 })} alt="" />
-    	</div>
+
+        <div>
+            <div class="group relative items-center justify-center overflow-hidden">
+                <div class="absolute duration-500 group-hover:translate-y-[-100%]">
+                    <img class="h-auto max-w-full" src={getImageLink({ id: 'qucYweSE3ZY', w: 1280, h: 1280 })} alt="" />
+                </div>
+
+
+                    <div class="h-96 max-h-full bg-primary-500 flex flex-col items-center justify-center text-center">
+                        <p class="text-white">Project-1</p>
+                    </div>
+
+            </div>
+        </div>
+
+
+
+
+
+
+
+
     	<div>
     		<img class="h-auto max-w-full rounded-lg" src={getImageLink({ id: 'qucYweSE3ZY', w: 1280, h: 1280 })} alt="" />
     	</div>
---
+++ my-new-portfolio/src/routes/projects/project-1/+page.svelte
@@ -0,0 +1,3 @@
+<div>
+    <h3 class="h1">This is project-1 page!</h3>
+</div>
