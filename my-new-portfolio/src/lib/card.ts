69a6180ae51c2f5eb5613d0e3f11d916bb954663
Subeom Jeong <92135821+MoebusChris@users.noreply.github.com>
2023-09-17 22:54:57 -0700

    Make dynamic render in the Projects page.

--- my-new-portfolio/src/lib/card.ts
+++ my-new-portfolio/src/lib/card.ts
@@ -1,31 +1,28 @@
-type Card = {
-    title: string;
-    id: string[];
-}
+// card.ts
 
-export const cardLists: Card[] = [
+export const cardLists = [
     {
         title: 'Development',
-        id: [
-                'tQ4fEcKfB1g',
-                'eP2zYflRoNY',
-                'EkW1r_eDjRs',
-        ]
-    },
-    {
+        list: [
+          { href: '/projects/project-1', name: 'project-1', imageId: 'tQ4fEcKfB1g' },
+          { href: '/projects/project-2', name: 'project-2', imageId: 'tQ4fEcKfB1g' },
+          { href: '/projects/project-3', name: 'project-3', imageId: 'tQ4fEcKfB1g' },
+        ],
+      },
+      {
         title: 'Design',
-        id: [
-                'tQ4fEcKfB1g',
-                'eP2zYflRoNY',
-                'EkW1r_eDjRs',
-        ]
-    },
-    {
-        title: 'User Experience',
-        id: [
-                'tQ4fEcKfB1g',
-                'eP2zYflRoNY',
-                'EkW1r_eDjRs',
-        ]
-    },
+        list: [
+          { href: '/projects/project-4', name: 'project-4', imageId: 'tQ4fEcKfB1g' },
+          { href: '/projects/project-5', name: 'project-5', imageId: 'tQ4fEcKfB1g' },
+          { href: '/projects/project-6', name: 'project-6', imageId: 'tQ4fEcKfB1g' },
+        ],
+      },
+      {
+        title: 'Development',
+        list: [
+          { href: '/projects/project-7', name: 'project-7', imageId: 'tQ4fEcKfB1g' },
+          { href: '/projects/project-8', name: 'project-8', imageId: 'tQ4fEcKfB1g' },
+          { href: '/projects/project-9', name: 'project-9', imageId: 'tQ4fEcKfB1g' },
+        ],
+      },
 ]
--- my-new-portfolio/src/routes/partials/ProjectsDev.svelte
+++ my-new-portfolio/src/routes/partials/ProjectsDev.svelte
@@ -1,40 +1,43 @@
 <script lang="ts">
     import { getImageLink } from "$lib/images";
     import { cardLists } from "$lib/card";
-    import { slide } from "svelte/transition";
 
 
 
 </script>
 
 <div class="space-y-4 md:space-y-8">
+    {#each cardLists as cardList, i}
+        
+    <header class="space-y-4">
+        <h2 class="h2">
+            {cardList.title}
+        </h2>
+    </header>
+    
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {#each cardList.list as { href, name, imageId }}
+        <a {href} class="group aspect-square rounded-container-token overflow-hidden shadow-xl variant-filled-primary">
+            <img class="w-full aspect-square transition-all duration-[500] group-hover:-translate-y-[101%]" src={getImageLink({ id: imageId, w: 1280, h: 1280 })} alt="" />
+            <caption class="w-full h-full flex justify-center items-center group-hover:-translate-y-full">{name}</caption>
+        </a>
+        {/each}
+    </div>
+    {/each}
+</div>
+
+<!-- For Future Reference-->
+<!-- <div class="space-y-4 md:space-y-8">
     <header class="space-y-4">
         <h2 class="h2">
             Development
         </h2>
     </header>
     
-    <div class="grid grid-cols-3 md:grid-cols-3 gap-4 py-8">
+    <div class="grid grid-cols-3 md:grid-cols-3 gap-4">
         <a href="/" class="group aspect-square rounded-container-token overflow-hidden shadow-xl variant-filled-primary">
                 <img class="w-full aspect-square transition-all duration-[500] group-hover:-translate-y-[101%]" src={getImageLink({ id: 'qucYweSE3ZY', w: 1280, h: 1280 })} alt="" />
                 <caption class="w-full h-full flex justify-center items-center group-hover:-translate-y-full">Project-1</caption>
         </a>
     </div>
-</div>
-
-
-
-
-<div class="py-6">
-    {#each cardLists as cardList, i}
-        <h3 class="h2">{cardList.title}</h3>
-        <section class="grid grid-cols-2 md:grid-cols-3 gap-4 py-8">
-            {#each cardList.id as imageId}
-                <div>
-                    <img class="h-auto max-w-full rounded-lg" src={getImageLink({ id: imageId, w: 1280, h: 1280 })} alt="" />
-                </div>
-            {/each}
-        </section>
-    {/each}
-</div>
-
+</div> -->
