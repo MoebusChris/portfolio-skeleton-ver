5dad046a951c6e7f666c018a1f23ac06b329afb5
Subeom Jeong <92135821+MoebusChris@users.noreply.github.com>
2023-10-15 13:00:04 -0700

    Adding meta data function

--- my-new-portfolio/src/routes/+layout.svelte
+++ my-new-portfolio/src/routes/+layout.svelte
@@ -14,6 +14,8 @@
 	import { afterNavigate } from '$app/navigation';
 
 	// Components & Utility
+	import { browser } from '$app/environment';
+	import { page } from '$app/stores';
 	import { prefersReducedMotionStore } from '@skeletonlabs/skeleton';
 
 	// My Components
@@ -28,6 +30,23 @@
 	import '@fortawesome/fontawesome-free/css/fontawesome.css';
 	import '@fortawesome/fontawesome-free/css/brands.css';
 	import '@fortawesome/fontawesome-free/css/solid.css';
+
+	// SEO Meta Tags
+	const metaDefault = {
+		title: 'Chris Subeom portfolio profile',
+		description: 'Im Chris Subeom, financial accounting professional + fullstack developer.',
+		image:'',
+	};
+	const meta = {
+		title: metaDefault.title,
+		description: metaDefault.description,
+		image: metaDefault.image,
+		linkedin: {
+			title: metaDefault.title,
+			description: metaDefault.description,
+			image: metaDefault.image,
+		}
+	}
 
 	// Initialize Store for Modals and Drawers
 	import { initializeStores } from '@skeletonlabs/skeleton';
@@ -52,9 +71,41 @@
 		scrollHeadingIntoView();
 	});
 
+	page.subscribe((page) => {
+		// Restore page Defaults
+		meta.title = metaDefault.title;
+		meta.description = metaDefault.description;
+		meta.image = metaDefault.image;
+		meta.linkedin.title = metaDefault.title;
+		meta.linkedin.description = metaDefault.description;		
+		meta.linkedin.image = metaDefault.image;
+	})
+
+
 	// Reactive
 	$: allyPageSmoothScroll = !$prefersReducedMotionStore ? 'scroll-smooth' : '';
 </script>
+
+<svelte:head>
+	<title>{meta.title}</title>
+	<!-- Meta Tags -->
+	<meta name="title" content={meta.title} />
+	<meta name="description" content={meta.description}/>
+	<meta name="keywords" content="finance, accounting, contents, creation, gaap, corporation, bookkeeping, audit, fullstack, development, javascript, typescript, python, quickbooks" />
+	<meta name="author" content="Christopher Subeom Jeong Greyson" />
+	<!-- Open Graph - https://opg.me/ -->
+	<meta property="og:site_name" content="I'm Chris Subeom" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://www.subeomjeong.com" />
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:title" content={meta.title} />
+	<meta property="og:description" content={meta.description} />
+	<meta property="og:image" content={meta.image} />
+	<meta property="og:image:secure_url" content={meta.image} />
+	<meta property="og:image:type" content="image/jpg" />
+	<meta property="og:image:width" content="" />
+	<meta property="og:image:height" content="" />
+</svelte:head>
 
 <!-- Overlay -->
 <DocsDrawer />
